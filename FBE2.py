a0ee7e128b007d31d40046e9e87584344061c5e4
intoro <tmcs.brian@gmail.com>
2017-03-24 19:38:10 -0700

    cookies work

--- FBE2.py
+++ FBE2.py
@@ -50,50 +50,37 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
 @app.route("/")
 def home():
     # get customised headlines, based on user input or default
-    publication = request.args.get("publication")
-    if not publication:
-        publication = request.cookies.get("publication")
-        if not publication:
-            publication = DEFAULTS["publication"]
-    # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = request.cookies.get('city')
-        if not city:
-            city = DEFAULTS['city']
-    weather = get_weather(city)
-    # get customized currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = request.cookies.get("currency_from")
-        if not currency_from:
-            currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = request.cookies.get("currency_to")
-        if not currency_to:
-          currency_to=DEFAULTS['currency_to']
-    rate, currencies = get_rate(currency_from, currency_to)
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
 
-    # Cookie
-    resp = make_response(render_template("home.html", articles=articles, weather=weather,
-                                              currency_from=currency_from,
-                                              currency_to=currency_to,
-                                              rate=rate,
-                                              currencies=sorted(currencies)))
+    # get customised weather based on user input or default
+    city = get_value_with_fallback("city")
+    weather = get_weather (city)
+
+    # get customised currency based on user input or default
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
+    rate, currencies = get_rate(currency_from, currency_to)
+
+    # save cookies and return template
+    response = make_response(render_template("home.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
     expires = datetime.datetime.now() + datetime.timedelta(days=365)
-    resp.set_cookie("publication", publication, expires=expires)
-    resp.set_cookie("city", city, expires=expires)
-    resp.set_cookie("currency_from", currency_from, expires=expires)
-    resp.set_cookie("currency_to", currency_to, expires=expires)
-
-    return resp
-
-
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+    
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
         publication = DEFAULTS["publication"]
